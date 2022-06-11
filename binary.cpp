9d4f20a745c815c5ec946466a92fefa937048bb5
jevs-pr <ghostraidenpm@gmail.com>
2022-06-11 01:41:38 -0400

    Binary

--- binary.cpp
+++ binary.cpp
@@ -5,28 +5,38 @@
 
 int main()
 {
-	input();
+	menu();
 	std::cout << "\n\n";
 	return 0;
 }
 
-void input()
-{
-	system("cls");
-	number numbertobinary;
-	int inputnumber;
-
-	std::cout << ("What number you want to convert to binary number : ");
-	std::cin >> inputnumber;
-
-	numbertobinary.setnumber(inputnumber);
-
-	std::cout << "\n";
-	std::cout << "The binary number of ";
-	std::cout << numbertobinary.getnumber() << " is " << numbertobinary.getBinary();
-	std::cout << "\n\n";
-	end();
-}
+void menu()
+{ 
+	int opc{0};
+	std::cout << "1.Number to Binary | 2.Binary to Number | 3.Word to Binary | 4.Binary to Word | 5.Exit" << std::endl;
+	opc = valid();
+	switch (opc)
+	{
+	case 1:
+		numberToBinary();
+		break;
+	case 2:
+		binarytonumber();
+		break;
+	case 3:
+		wordToBinary();
+		break;
+	case 4:
+		binaryToWord();
+	case 5:
+		return;
+	default:
+		system("cls");
+		std::cout << "Error : 1 , 2 , 3 , 4 or 5" << std::endl;
+		menu();
+		break;
+	}
+} 
 
 void end()
 {
@@ -36,7 +46,8 @@ void end()
 	switch (std::toupper(inputEnd))
 	{
 	case 'Y':
-		input();
+		system("cls");
+		menu();
 		break;
 	case 'N':
 
@@ -47,20 +58,223 @@ void end()
 	}
 }
 
-void number::setnumber(int set)
+void message(std::string message, std::string num1, std::string num2)
 {
-	number = set;
-	int result = number;
-	
-	while (result != 0)
+	std::cout << "\n";
+	std::cout << message;
+	std::cout << num1 << " is: \n\n" << num2;
+	std::cout << "\n\n";
+	end();
+}
+
+int valid()
+{
+	int num;
+
+	while (true) {
+		if (std::cin >> num) {
+			break;
+		}
+		else {
+			std::cout << "Enter a valid integer value!\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+	return num;
+
+}
+
+void validBinary(std::string binary , int opc)
+{
+	for (int i = 0; i < binary.length(); i++)
 	{
-		binary = binary + std::to_string(result % 2);
-		result = result / 2;
+		if (opc == 4 && binary.length() % 8 != 0)
+		{
+			system("cls");
+			std::cout << "Error : Some words are incomplete\n";
+			system("pause");
+			system("cls");
+			menu();
+		}
+
+		if (binary[i] == '0' || binary[i] == '1' || binary[i] == ' ')
+		{
+		}
+		else
+		{
+			system("cls");
+			std::cout << "Error : Only 0 or 1\n";
+			system("pause");
+			system("cls");
+			switch (opc)
+			{
+			case 2:
+				binarytonumber();
+				break;
+			case 4:
+				binaryToWord();
+				break;
+			}
+		}
+	}
+}
+
+/// Number to Binary
+
+void numberToBinary()
+{
+	system("cls");
+	int inputnumber;
+	std::cout << ("What number you want to convert to binary number : ");
+	inputnumber = valid();
+
+	number numbertobinary(inputnumber);
+
+	numbertobinary.numberToBinaryCall();
+
+	message("The binary number of ", std::to_string(inputnumber) , numbertobinary.getBinary());
+} 
+
+void number::numberToBinaryCall()
+{
+	numberToBinaryFormula(_number, _binary);
+} 
+
+void number::numberToBinaryFormula(int& number ,std::string& binary)
+{
+	while (number != 0)
+	{
+		binary = binary + std::to_string(number % 2);
+		number = number / 2;
 	}
 
-	for (int i = 0 ;  i < binary.length()/2; i++)
+	for (int i = 0; i < binary.length() / 2; i++)
 	{
 		std::swap(binary[i], binary[binary.length() - i - 1]);
 	}
 }
 
+/// Binary to Number
+
+void binarytonumber()
+{
+	std::string binarynumber;
+	std::cout << "What binary number you want to convert to decimal? ";
+	std::cin >> binarynumber;
+	validBinary(binarynumber,2);
+	number inputnumber(binarynumber);
+
+	inputnumber.binaryToNumberCall();
+
+	message("The number of ", binarynumber, std::to_string(inputnumber.getNumber()));
+
+} 
+
+void number::binaryToNumberCall()
+{
+	binaryToNumberFormula(_number,_binary);
+}
+
+void number::binaryToNumberFormula(int& number, std::string& binary)
+{
+	for (int i = 0; i < binary.length() / 2; i++)
+	{
+		std::swap(binary[i], binary[binary.length() - i - 1]);
+	}
+
+	int result = 0;
+
+	for (int i = 0; i < binary.length(); i++)
+	{
+		int num = binary[i] - '0';
+		result += num * pow(2, i);
+	}
+
+	number = result;
+}
+
+/// Words to Binary
+
+void wordToBinary() {
+	std::cout << ("What word you want to convert to binary number : ");
+	std::string word{};
+	std::cin.ignore();
+	std::getline(std::cin,word);
+	number inputword(word, true);
+	
+	inputword.stringToBinaryCall();
+
+	message("The binary number of ", inputword.getWord(), inputword.getBinary());
+}
+
+void number::stringToBinaryCall()
+{
+	int ascii;
+	std::string resultLettle;
+	std::string resultWord;
+
+	for (int i = 0; i < _word.length(); i++)
+	{
+		resultLettle = "";
+		ascii = _word[i];
+		
+		numberToBinaryFormula(ascii, resultLettle);
+
+		resultWord += "0" + resultLettle + " ";
+	}
+	_binary = resultWord;
+}
+
+/// Binary to Words
+
+void binaryToWord()
+{
+	std::string binary{"01001000 01101001"};
+	std::cout << ("What Binary you want to convert to Words : ");
+	std::cin.ignore();
+	std::getline(std::cin, binary);
+	number inputBinary(binary);
+	inputBinary.binaryToStringCall();
+
+	message("The word of ", inputBinary.getBinary(), inputBinary.getWord());
+}
+
+void number::binaryToStringCall()
+{
+	std::string lineBinary{""};
+	std::vector<std::string> arrayBinary;
+	std::vector<int> arrayChar;
+	for (int i = 0; i < _binary.length(); i++)
+	{
+		if (_binary[i] != ' ')
+			lineBinary += _binary[i];
+	} //Removing spaces from the string
+
+	validBinary(lineBinary, 4);
+
+	for (int i = 0; i < lineBinary.length() / 8; i++)
+	{
+		arrayBinary.push_back("");
+		arrayChar.push_back(0);
+	} //Creating space in array
+
+	int position = 0;
+	for (int i = 0; i < lineBinary.length(); i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			position++;
+		}
+		arrayBinary[position] += lineBinary[i];
+	}//Set the posicion vector with lineBinary
+
+	for (int i = 0; i < arrayBinary.size(); i++)
+	{
+		binaryToNumberFormula(arrayChar[i], arrayBinary[i]);
+	}
+	for (int i = 0; i < arrayBinary.size(); i++)
+	{
+		_word += char(arrayChar[i]);
+	}
+}
--- binary.h
+++ binary.h
@@ -4,17 +4,49 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <math.h>
+#include <vector>
 
 class number
 {
 private:
-	int number;
-	std::string binary{};
+
+	//Variable
+
+	int _number;
+	std::string _binary{};
+	std::string _word{};
 public:
-	void setnumber(int set);
-	int getnumber() { return number; }
-	std::string getBinary() { return binary; }
+
+	//Contruct
+
+	number(int setNumber) { _number = setNumber; }
+	number(std::string setString) { _binary = setString; }
+	number(std::string setWord , bool word) { _word = setWord; }
+
+	//Function
+
+	int getNumber() { return _number; }
+	std::string getBinary() { return _binary; }
+	std::string getWord() { return _word; }
+
+	void numberToBinaryCall();
+	void numberToBinaryFormula(int& number, std::string& binary);
+
+	void binaryToNumberCall();
+	void binaryToNumberFormula(int& number, std::string& binary);
+	
+	void stringToBinaryCall();
+
+	void binaryToStringCall();
 };
 
-void input();
+void menu();
 void end();
+void message(std::string message , std::string num1 , std::string num2);
+int valid();
+void validBinary(std::string binary , int opc);
+void numberToBinary();
+void binarytonumber();
+void wordToBinary();
+void binaryToWord();
